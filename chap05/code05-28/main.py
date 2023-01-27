7e7210fc5ad065db0e1787b09a67927528bf8910
Shinpei KUDO <kudo@miyabilink.jp>
2023-01-27 14:13:13 +0900

    初版用dokoPyライブラリ準備（5章）

---
+++ chap05/code05-28/main.py
@@ -0,0 +1,13 @@
+# このコードを実行するには、関数定義も実行する必要があります。
+
+"""
+# コード5-27
+def eat(breakfast, lunch, dinner='カレー', desserts=()):
+    print('朝は{}を食べました'.format(breakfast))
+    print('昼は{}を食べました'.format(lunch))
+    print('晩は{}を食べました'.format(dinner))
+    for d in desserts:
+        print('おやつに{}を食べました'.format(d))
+"""
+
+eat('トースト', 'パスタ', 'カレー', ('アイス', 'チョコ', 'パフェ'))
