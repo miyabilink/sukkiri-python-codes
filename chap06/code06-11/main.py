df97cae36e291e386125470382fc1ddd3e5a33c4
Shinpei KUDO <kudo@miyabilink.jp>
2023-01-27 14:48:23 +0900

    初版用dokoPyライブラリ準備（6章）

---
+++ chap06/code06-11/main.py
@@ -0,0 +1,9 @@
+names = list() # リストの場合
+print('変更前のlistのidentity: {}'.format(id(names)))
+names.append('松田')
+print('変更後のlistのidentity: {}'.format(id(names)))
+
+name = '松田'   # 文字列の場合
+print('変更前のstrのidentity: {}'.format(id(name)))
+name = 'スーパー' + name
+print('変更後のstrのidentity: {}'.format(id(name)))
