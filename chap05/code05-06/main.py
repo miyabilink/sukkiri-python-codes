bebd5737ec76b72a68c410589b72c29392128ffc
Shinpei KUDO <kudo@miyabilink.jp>
2023-01-27 14:13:13 +0900

    初版用dokoPyライブラリ準備（5章）

---
+++ chap05/code05-06/main.py
@@ -0,0 +1,13 @@
+# このコードを実行するには、関数定義も実行する必要があります。
+
+"""
+# コード5-5
+def input_scores():
+    name = ''
+    print('{}の試験結果を入力してください'.format(name))
+"""
+
+name = '浅木'
+input_scores()
+name = '松田'
+input_scores()
---
+++ chap05/code05-34/main.py
@@ -0,0 +1,13 @@
+# このコードを実行するには、関数定義も実行する必要があります。
+
+"""
+# コード5-33
+name = '松田'
+def change_name():
+    name = '浅木'
+def hello():
+    print('こんにちは' + name + 'さん')
+"""
+
+change_name()
+hello()
