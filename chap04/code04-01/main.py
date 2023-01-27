b0c583c6231d430dc8fc690bf2afa67710ed28a9
Shinpei KUDO <kudo@miyabilink.jp>
2023-01-27 10:06:49 +0900

    初版用dokoPyライブラリ準備（4章まで）

---
+++ chap04/code04-01/main.py
@@ -0,0 +1,9 @@
+print('さあ、寝ようかしら')
+count = 0   # ひつじの数
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+print('おやすみなさい...')
